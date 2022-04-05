d4553618f117e39f14ef44226622befce259bd13
Tylee09 <93730358+Tylee09@users.noreply.github.com>
2022-04-05 11:49:07 -0400

    Add files via upload

---
+++ pain.py
@@ -0,0 +1,4 @@
+first_number = int(input('what is the first number you want to add?'))
+second_number = int(input("What is the secont dumber you want to add?"))
+sum = first_number + second_number
+print('The sum of your two numbers is',sum)
